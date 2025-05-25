1ae01bfc3db9340e82c4c8f846cabf6719152b65
Vitalii <vitalii.turych@gmail.com>
2025-05-25 13:32:07 +0300

    CarWashStation solutions 2

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ class CarWashStation:
                  / self.distance_from_city_center)
         return round(price, 1)
 
-    def wash_single_car(self, car: Car) -> None:
+    def wash_single_car(self, car: list[Car]) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
